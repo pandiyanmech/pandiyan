082ff83a3f3b2a4d365602e04088cc569777c361
pandiyanmech <35764854+pandiyanmech@users.noreply.github.com>
2018-03-07 16:01:36 +0530

    demo

---
+++ demo1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+main class large number ()
+{
+public static void main(String[]agrs)
+int max,i;
+int arr[10]={10,20,30,40,50};
+max=arr[0];
+for(i=0;i<arr.length;i++)
+{
+if(arr[i]>max)
+{
+max=arr[i];
+}
+}
+System.out.println("The largest number is",+max);
+}
+getch();
+return();
+}
